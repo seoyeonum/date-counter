9b1925c396e5408616e23b500f5fd083a93c5af9
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-09-23 21:03:15 +0900

    ✨ feat: complete date-counter functionality
    - use toDateString() for date display instead of using getFullYear()
    - remove nameOfDay and nameOfMonth arrays
    - implement date count function with getDate() and setDate()
    - simplify variables in setCount and setStep
    - use ternary operator for past, present, and future messages
    - add span elements for structured display

--- src/App.js
+++ src/App.js
@@ -9,76 +9,55 @@ export default function App() {
   );
 }
 
-const nameOfDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const nameOfMonth = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec',
-];
-
 function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
+  // get today's data
+  const today = new Date();
+  today.setDate(today.getDate() + count);
+
   // step handler: minus
   function handleMinusStep() {
-    if (step > 1) setStep((step) => step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   // step handler: plus
   function handlePlusStep() {
-    setStep((step) => step + 1);
+    setStep((s) => s + 1);
   }
 
   // count handler: minus
   function handleMinusCount() {
-    setCount((count) => count - step);
+    setCount((c) => c - step);
   }
 
   // count handler: plus
   function handlePlusCount() {
-    setCount((count) => count + step);
+    setCount((c) => c + step);
   }
-
-  // const date1 = new Date(); // new Date() 는 Object
-  // console.log(date1);
-  // console.log(typeof date1);
-  // const date2 = Date(); // Date() 는 string
-  // console.log(date2);
-  // console.log(typeof date2);
-
-  // get today's data
-  const today = new Date();
-
-  const day = today.getDay(); // .getDay() 는 0(일) ~ 6(토)로 출력된다.
-  const date = today.getDate();
-  const month = today.getMonth(); // .getMonth() 는 0 ~ 11 로 출력된다.
-  const year = today.getFullYear();
 
   return (
     <>
       <div>
         <Button btnName="-" functionName={handleMinusStep} />
-        Step: {step}
+        <span>Step: {step}</span>
         <Button btnName="+" functionName={handlePlusStep} />
       </div>
       <div>
         <Button btnName="-" functionName={handleMinusCount} />
-        Count: {count}
+        <span>Count: {count}</span>
         <Button btnName="+" functionName={handlePlusCount} />
       </div>
       <p>
-        {count} days from today is {nameOfDay[day]} {nameOfMonth[month]} {date}{' '}
-        {year}
+        <span>
+          {count === 0
+            ? 'Today is '
+            : count > 0
+            ? `${count} days from today is `
+            : `${count} days ago was `}
+          {today.toDateString()}
+        </span>
       </p>
     </>
   );
